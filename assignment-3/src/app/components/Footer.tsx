91084c455c179ce68d38c41fb662e5d288946e69
Ahmed-Raza0 <ahed80174@gmail.com>
2024-10-17 22:05:02 +0500

    assignment 3

---
+++ assignment-3/src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center bg-black text-white   ">
+            <p >Copyright &copy; 2024</p>
+        </footer>
+    )
+}
---
+++ assignment-3/src/app/components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="bg-black text-white p-4 md:p-5 w-full fixed top-0 z-50 flex justify-between items-center shadow-md">
+      <h1 className="text-2xl font-bold">Header</h1>
+      <div className="flex space-x-4">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+        <Link href="/contact" className="hover:underline">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+}
