6da2429b807982546371c7ab6cb4783f5f38185b
Ahmed-Raza0 <ahed80174@gmail.com>
2024-10-24 23:45:10 +0500

    Assignment-4

---
+++ assignment-4/src/app/componenets/Footer.tsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+    return (
+      <footer className="bg-black text-white py-4">
+        <div className="max-w-7xl mx-auto text-center">
+          &copy; {new Date().getFullYear()} Ahmed Raza. All rights reserved.
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ assignment-4/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "./componenets/Footer";
+import Header from "./componenets/Header";
+
+export default function Home() {
+  return (
+      <main className="bg-black">
+        <Header />
+        <Footer />
+      </main>
+  );
+}
